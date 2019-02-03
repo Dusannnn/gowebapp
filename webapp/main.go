ab44914ee635bd8bc942d48380a0d7a476f92709
Dusan <dusanpetren@outlook.com>
2019-02-03 22:34:13 +0100

    initCommint

--- webapp/main.go
+++ webapp/main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8000", nil)
-}
+	a := App{}
+	a.Initialize("root", "admin123", "test")
 
-func handlerFunc(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "<h1>Welcome to my site!</h1>")
+	a.Run(":80")
 }
